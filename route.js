1afe07d80080ee703ef6d37274cd7946eced8991
Rob Nolen <rnolen@trace3.com>
2015-02-14 00:25:28 -0800

    fixed routing of dead files, now 40now 404 if can't stat file on fs or not in routing

--- route.js
+++ route.js
@@ -1,8 +1,15 @@
+var fs = require("fs");
 function route(pathname) {
-    var file = "";
-    console.log("routing request for " + pathname);
+    var file = "." + pathname;
+    console.log("routing request for " + file);
     if (pathname == "/") {
         file = "./index.html";
+    }
+    else if (fs.existsSync(file)) {
+        file = "." + pathname;
+    }
+    else {
+        file = "./404.html";
     }
     return file;
 }
