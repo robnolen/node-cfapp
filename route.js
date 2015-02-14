55e044415c20d51e8ec205ed478c27e4f2ba0d31
Rob Nolen <rnolen@trace3.com>
2015-02-13 23:24:34 -0800

    changes trying to route pathname

--- route.js
+++ route.js
@@ -1,5 +1,15 @@
-function route(pathname) {
+var fs = require("fs");
+
+function route(response, pathname) {
     console.log("routing request for " + pathname);
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    if (pathname == "/") {
+        fs.createReadStream('./index.html').pipe(response);
+    }
+    else{
+        response.write("no path");
+    }
+    response.end();
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -6,10 +6,7 @@ function start(route) {
         console.log("Request received.");
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received");
-        route(pathname);
-        response.writeHead(200, {"Content-Type" : "text/plain"});
-        response.write("Hello World");
-        response.end();
+        route(response, pathname);
     }
     
     http.createServer(onRequest).listen(process.env.VCAP_APP_PORT);
