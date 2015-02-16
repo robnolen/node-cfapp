ac87c5c9cfb41fe880725f81d1dda6d4ea590ee0
Rob Nolen <rnolen@trace3.com>
2015-02-15 21:35:46 -0800

    basic rest api consumption complete

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ var http   = require("http"),
     url    = require("url"),
     fs     = require("fs"),
     Router = require("router"),
-    st     = require("st")
-
+    st     = require("st"),
+    getApi = require("./getapi");
 function start() {
 
     var router = Router();
@@ -12,6 +12,11 @@ function start() {
                      url: '/', 
                      index: 'index.html'});
 
+    router.get("/getapi", function(req, res) { 
+        console.log("getapi");
+        getApi.getapi(req, res);
+    });
+    
     var server = http.createServer(function(req, res) {
         router(req, res, function() { 
             mount(req, res);
