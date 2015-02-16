3112c5f7fb760578773129a096559942afbc18a5
Rob Nolen <rnolen@trace3.com>
2015-02-15 19:45:46 -0800

    removed router entry for /, was breaking index.html

--- src/server.js
+++ src/server.js
@@ -12,12 +12,10 @@ function start() {
                      url: '/', 
                      index: 'index.html'});
 
-    router.get('/', function(req, res) {
-        //mount(req, res);
-    });
-
     var server = http.createServer(function(req, res) {
-        router(req, res, function() { mount(req, res);});
+        router(req, res, function() { 
+            mount(req, res);
+        });
     });
     server.listen(process.env.VCAP_APP_PORT);
 }
