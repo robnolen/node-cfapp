59dd817234aa13e4127a564d9a83874d973676bb
Rob Nolen <rnolen@trace3.com>
2015-02-17 22:26:04 -0800

    fixed jade template

--- src/testjade.js
+++ src/testjade.js
@@ -1,10 +1,11 @@
 var jade = require('jade');
 
 exports.testjade = function (req, res) {
+    var jade_opts = {"debug" : "true", "pretty" : true};
     var jadeTemplate = __dirname + "/templates/test.jade";
     var notes = [{ "id": 1, "title": "Jogging in park" }, { "id": 2, "title": "Pick-up posters from post-office" }];
-    var fn = jade.compileFile(jadeTemplate, {});
-    var html = fn(notes);
+    var fn = jade.compileFile(jadeTemplate, jade_opts);
+    var html = fn({"notes" :notes});
     console.log("heh");
     res.writeHead(200, {"Content-Type" : "text/html" });
     res.write(html);
