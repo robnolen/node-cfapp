b2279757b875841f7aaaafc29f914bfa59ab5364
Rob Nolen <rnolen@trace3.com>
2015-02-17 21:56:38 -0800

    trying to set up jade iteration

--- src/testjade.js
+++ src/testjade.js
@@ -1,10 +1,12 @@
 var jade = require('jade');
 
 exports.testjade = function (req, res) {
-    var jadeTemplate = "templates/test.jade";
-    var fn = jade.renderFile(jadeTemplate, {});
+    var jadeTemplate = __dirname + "/templates/test.jade";
+    var notes = [{ "id": 1, "title": "Jogging in park" }, { "id": 2, "title": "Pick-up posters from post-office" }];
+    var fn = jade.compileFile(jadeTemplate, {});
+    var html = fn(notes);
     console.log("heh");
     res.writeHead(200, {"Content-Type" : "text/html" });
-    res.write("fuck");
+    res.write(html);
     res.end();
     }
